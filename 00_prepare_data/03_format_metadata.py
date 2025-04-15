932889aee8d65fce3168a39ef7dbb96381066613
Jess Ewald <jewald@ebi.ac.uk>
2025-04-15 14:16:38 -0400

    merge metadata with OASIS annotations

--- 00_prepare_data/03_format_metadata.py
+++ 00_prepare_data/03_format_metadata.py
@@ -52,15 +52,15 @@ def main() -> None:
     meta = meta.rename({
         "Metadata_plate_map_name": "Metadata_Plate",
         "Metadata_well_position": "Metadata_Well",
-        "Metadata_BatchId": "Metadata_Compound",
+        "Metadata_BatchId": "Metadata_Broad_ID",
         "Metadata_Concentration(uM)": "Metadata_Concentration",
     }).with_columns(
         pl.when(pl.col("Metadata_Concentration").is_null()).then(pl.lit(0)).otherwise(pl.col("Metadata_Concentration")).alias("Metadata_Concentration")
     ).with_columns(
-        pl.concat_str(["Metadata_Compound", "Metadata_Concentration"], separator="_").alias("Metadata_Perturbation"),
+        pl.concat_str(["Metadata_Broad_ID", "Metadata_Concentration"], separator="_").alias("Metadata_Perturbation"),
     )
 
-    compounds = meta.filter(pl.col("Metadata_Concentration") != 0).select("Metadata_Compound").to_series().unique().to_list()
+    compounds = meta.filter(pl.col("Metadata_Concentration") != 0).select("Metadata_Broad_ID").to_series().unique().to_list()
 
     meta_log10 = meta.filter(pl.col("Metadata_Concentration") == 0).with_columns(
         pl.lit(0).cast(pl.Float64).alias("Metadata_Log10Conc"),
@@ -68,7 +68,7 @@ def main() -> None:
 
     # Convert non-zero concentrations to log-scale and shift to above zero
     for compound in compounds:
-        temp = meta.filter(pl.col("Metadata_Compound") == compound)
+        temp = meta.filter(pl.col("Metadata_Broad_ID") == compound)
         concs = temp.select(pl.col("Metadata_Concentration")).to_series().sort().unique().to_list()
 
         if len(concs) > 1:
@@ -85,10 +85,37 @@ def main() -> None:
         meta_log10 = pl.concat([meta_log10, temp], how="vertical")
 
     # need to merge with OASIS metadata
-    # rename blank compound as DMSO
+    id_map = pl.read_csv("../01_snakemake/inputs/annotations/OASIS_BRDID_map.csv").select([
+        "OASIS_ID", "BROAD_ID"
+    ]).rename({"OASIS_ID": "Metadata_OASIS_ID", "BROAD_ID": "Metadata_Broad_ID"})
+
+    oasis = pl.read_csv("../01_snakemake/inputs/annotations/v5_oasis_03Sept2024_simple.csv").select([
+        "OASIS_ID", "DTXSID", "PREFERRED_NAME", "INCHIKEY", "Purchased_Axiom_Medchemxpress"
+    ]).rename({
+        "OASIS_ID": "Metadata_OASIS_ID",
+        "DTXSID": "Metadata_DTXSID",
+        "PREFERRED_NAME": "Metadata_Compound",
+        "INCHIKEY": "Metadata_Inchikey",
+        "Purchased_Axiom_Medchemxpress": "Metadata_Axiom"
+    }).filter(~pl.col("Metadata_OASIS_ID").is_null()).unique()
+
+    meta_log10 = meta_log10.join(id_map, on="Metadata_Broad_ID", how="left")
+    meta_log10 = meta_log10.join(oasis, on="Metadata_OASIS_ID", how="left")
+
+    meta_log10 = meta_log10.with_columns(
+        pl.when(pl.col("Metadata_Concentration") == 0).then(pl.lit("DMSO")).when((pl.col("Metadata_Concentration") > 0) & (pl.col("Metadata_Compound").is_null())).then(pl.lit("JUMP_control")).otherwise(pl.lit("OASIS_cmpd_exposure")).alias("Metadata_well_type")
+    )
+
+    # rename blank compound as DMSO or BRD ID
+    meta_log10 = meta_log10.with_columns(
+        pl.when(pl.col("Metadata_Concentration") == 0).then(pl.lit("DMSO")).otherwise(pl.col("Metadata_Compound")).alias("Metadata_Compound")
+    )
+
+    meta_log10 = meta_log10.with_columns(
+        pl.when(pl.col("Metadata_Compound").is_null()).then(pl.col("Metadata_Broad_ID")).otherwise(pl.col("Metadata_Compound")).alias("Metadata_Compound")
+    )
 
     meta_log10.write_parquet("../01_snakemake/inputs/metadata/metadata.parquet")
-
 
 if __name__ == "__main__":
     main()
